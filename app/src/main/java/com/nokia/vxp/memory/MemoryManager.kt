6244e2ecec122be130e8767bb9629e32e2e5944f
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2026-08-01 17:10:27 +0530

    first commit

--- app/src/main/java/com/nokia/vxp/memory/MemoryManager.kt
+++ app/src/main/java/com/nokia/vxp/memory/MemoryManager.kt
@@ -81,7 +81,7 @@ class MemoryManager : GuestMemoryReader {
         stackImpl = null
     }
 
-    fun read(address: Long, length: Int): ByteArray? {
+   override fun read(address: Long, length: Int): ByteArray? {
         if (!isEngineReady) {
             Logger.w(TAG, "read() called with no engine set up")
             return null
