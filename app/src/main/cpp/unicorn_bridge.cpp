efa2d597aa6b1fc4dd90cbc0a4e7b97a317a7efe
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2026-08-04 17:58:34 +0530

    04-08

--- app/src/main/cpp/unicorn_bridge.cpp
+++ app/src/main/cpp/unicorn_bridge.cpp
@@ -6,29 +6,30 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-uc_engine* vxp_create_arm_engine(bool thumb) {
+uc_engine* vxp_create_arm_engine() {
     uc_engine* uc = nullptr;
 
-    uc_mode mode = thumb ? UC_MODE_THUMB : UC_MODE_ARM;
-
-    uc_err err = uc_open(UC_ARCH_ARM, mode, &uc);
+    uc_err err = uc_open(
+        UC_ARCH_ARM,
+        UC_MODE_ARM,
+        &uc
+    );
 
     if (err != UC_ERR_OK) {
         LOGE("uc_open failed: %s", uc_strerror(err));
         return nullptr;
     }
 
-    LOGI("Unicorn engine created (%s mode)", thumb ? "THUMB" : "ARM");
+    LOGI("Unicorn ARM engine created");
 
     return uc;
 }
 
 void vxp_destroy_engine(uc_engine* uc) {
-    if (!uc)
+    if (uc == nullptr) {
         return;
-
+    }
     uc_err err = uc_close(uc);
-
     if (err != UC_ERR_OK) {
         LOGE("uc_close failed: %s", uc_strerror(err));
     } else {
